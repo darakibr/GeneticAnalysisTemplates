047d16d6940a754f6a6809ec92b32b2142450884
Daniel <darakibr@gmail.com>
2022-05-03 16:55:38 -0500

    Create new file

---
+++ TEMPLATE_genREF.py
@@ -0,0 +1,27 @@
+### Create a BACTERIAL_reference_files folder
+#Example is for Staphoria Aureus
+
+# create localDB folder
+if not found
+    create localDB folder
+else
+    do nothing
+
+# create BACTERIAL.fa
+if not found
+    search ncbi
+    download from ncbi
+else
+    do nothing
+
+# create header_mlst.txt file
+if not found
+    open
+else
+    do nothing
+
+# create BACTERIAL.txt file for MLST
+if not found
+    open
+else
+    do nothing
