956c3a4823d7db49d92521ba2da252ec5f9e4032
Daniel <darakibr@gmail.com>
2022-08-03 11:07:22 -0500

    Update template_ONTpipe.sh

--- template_ONTpipe.sh
+++ template_ONTpipe.sh
@@ -1,68 +1,57 @@
 #!/bin/bash
 
 ### modify these variables ###
-folder="ONT" #  >>> enter the name you want to give folder, will also dictate output names <<< #
+folder="GBS_ONT" #  >>> enter the name you want to give folder, will also dictate output names <<< #
 REFERENCE="REFISOLATE" # >>> enter the reference isolate name (should match .fa file name) <<< #
 threads="34"
+genomesize="2200000"
+current_time=$(date "+%y%m%d.%H%M")
 
 # cwd is: /home/Documents/JCM_SA
-### This will create the vcf files to be used to call SNP ###
-mkdir {hybrid} &&
-cd hybrid &&
+### This is the first step in TRYCYCLER PIPELINE, will output cluster data from ONT long read data ###
+mkdir -p Trycycler &&
+cd Trycycler &&
 
-for X in BCJB1835  ; do #For XXXX insert strain or accession numbers#
+for X in BCJB1835 BCJB2983 BCJB3343 BCJB3344 BCJB3586 BCJB4361 ; do #For XXXX insert strain or accession numbers#
 		cp ../fastq/"$X"_R1.fastq.gz .
 		cp ../fastq/"$X"_R2.fastq.gz .
-		cp ../../GBS_ONT/TypeIV/demultiplex/"$X"_ONT.fastq.gz
+		cp ../../"$folder"/TypeIV/demultiplex/"$X"_ONT.fastq.gz .
 		
 		#trim ONT reads to >1k and eliminate worst 5% of reads#
-		filtlong --min_length 5000 --keep_percent 95 "$X"_ONT.fastq.gz > $"X"_5k5perc.fastq
-		
-		#check read quality using raspberry
-		raspberry "$X"_5k5perc.fastq > "$X"_readqc.txt
-		#quick cleanup
-		rm *.rlen
+		filtlong --min_length 1000 --keep_percent 95 "$X"_ONT.fastq.gz > "$X"_1k5perc.fastq
 		
 		#Subsampling reads
-		trycycler subsample --reads "$X"_5k5perc.fastq --out_dir "$X"_subsets --min_read_depth 300 --threads "$threads"
+		trycycler subsample --reads "$X"_1k5perc.fastq --out_dir "$X"_subsets --min_read_depth 75 --count 20 --threads "$threads"
 		#Independent assemblies
-		threads="$threads"
-		mkdir "$X"_assemblies
+		mkdir -p "$X"_assemblies
 		
-		flye --nano-hq "$X"_subsets/sample_01.fastq --threads "$threads" --out-dir assembly_01 && cp assembly_01/assembly.fasta "$X"_assemblies/assembly_01.fasta && rm -r assembly_01
-		miniasm_and_minipolish.sh "$X"_subsets/sample_02.fastq "$threads" > assembly_02.gfa && any2fasta assembly_02.gfa > "$X"_assemblies/assembly_02.fasta && rm assembly_02.gfa
-		raven --threads "$threads" "$X"_subsets/sample_03.fastq > "$X"_assemblies/assembly_03.fasta && rm raven.cereal
-		
-		flye --nano-hq "$X"_subsets/sample_04.fastq --threads "$threads" --out-dir assembly_04 && cp assembly_04/assembly.fasta "$X"_assemblies/assembly_04.fasta && rm -r assembly_04
-		miniasm_and_minipolish.sh "$X"_subsets/sample_05.fastq "$threads" > assembly_05.gfa && any2fasta assembly_05.gfa > "$X"_assemblies/assembly_05.fasta && rm assembly_05.gfa
-		raven --threads "$threads" "$X"_subsets/sample_06.fastq > "$X"_assemblies/assembly_06.fasta && rm raven.cereal
-		
-		flye --nano-hq "$X"_subsets/sample_07.fastq --threads "$threads" --out-dir assembly_07 && cp assembly_07/assembly.fasta "$X"_assemblies/assembly_07.fasta && rm -r assembly_07
-		miniasm_and_minipolish.sh "$X"_subsets/sample_08.fastq "$threads" > assembly_08.gfa && any2fasta assembly_08.gfa > "$X"_assemblies/assembly_08.fasta && rm assembly_08.gfa
-		raven --threads "$threads" "$X"_subsets/sample_09.fastq > "$X"_assemblies/assembly_09.fasta && rm raven.cereal
-		
-		flye --nano-raw "$X"_subsets/sample_10.fastq --threads "$threads" --out-dir assembly_10 && cp assembly_10/assembly.fasta "$X"_assemblies/assembly_10.fasta && rm -r assembly_10
-		miniasm_and_minipolish.sh "$X"_subsets/sample_11.fastq "$threads" > assembly_11.gfa && any2fasta assembly_11.gfa > "$X"_assemblies/assembly_11.fasta && rm assembly_11.gfa
-		raven --threads "$threads" "$X"_subsets/sample_12.fastq > "$X"_assemblies/assembly_12.fasta && rm raven.cereal
-		#quick cleanup
-		rm -r "$X"_subsets
-		#Tricycler Culster using Mash distance
-		trycycler cluster --assemblies "$X"_assemblies/*.fasta --reads "$X"_5k5perc.fastq --out_dir trycycler --threads "$threads"
-		### THIS WILL PRODUCE cluster tree graphs, ideally they are very clean
-		### If there are 'messy' trees with lots of different lenght branches and strange leafs
-		### RENAME 'messy' clustering to start with 'bad_<cluster##>' to facilitate continuation of assembly.
-		
-		
-		
+		for i in 01 05 09 13 17; do
+			canu -p canu -d canu_temp -fast genomeSize="$genome_size" useGrid=false minThreads="$threads" maxThreads="$threads" -nanopore-corrected "$X"_subsets/sample_"$i".fastq -assemble
+			mv canu_temp/canu.contigs.fasta > "$X"_assemblies/"$X"assembly_"$i".fasta
+			rm -rf canu_temp
+		done
+		for i in 02 06 10 14 18; do
+			flye -g "$genomesize" --nano-corr "$X"_subsets/sample_"$i".fastq --threads "$threads" --out-dir temp_flye &&
+			cp temp_flye/assembly.fasta "$X"_assemblies/"$X"assembly_"$i".fasta &&
+			rm -r temp_flye
+		done
+		for i in 03 07 11 15 19; do
+			miniasm_and_minipolish.sh  "$X"_subsets/sample_"$i".fastq "$threads" > miniasm_temp.gfa
+			any2fasta miniasm_temp.gfa > "$X"_assemblies/"$X"assembly_"$i".fasta
+			rm miniasm_temp.gfa
+		done
+		for i in 04 08 12 16 20; do
+			raven --threads "$threads" --disable-checkpoints "$X"_subsets/sample_"$i".fastq > "$X"_assemblies/"$X"assembly_"$i".fasta
+		done
 		
 		#cleanup#
-		mv "$f"_ss.txt ./seeker/
-		mv "$f"_contigs-1K.fasta ./denovo/
-		mv "$f"_readqc.txt ./stats/
-		rm -r "$f"_spades-de-novo
-		rm *.fastq
-		rm *.fastq.gz
-		rm "$f"_contigs2.fasta
-		rm "$f"_contigs.fasta
+		rm "$X"_ONT.fastq.gz
+		rm -r "$X"_subsets
+		
+		#Tricycler Culster using Mash distance
+		trycycler cluster --assemblies "$X"_assemblies/*.fasta --reads "$X"_1k5perc.fastq --out_dir "$X"trycycler_out --threads "$threads"
+		### THIS WILL PRODUCE cluster tree graphs, ideally they are very clean
+		### If there are 'messy' trees with lots of different lenght branches and strange leafs
+		### RENAME 'messy' clustering to start with 'bad_<cluster##>_short' to facilitate continuation of assembly.
 		
 done
